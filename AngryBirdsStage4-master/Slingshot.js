352abbf9fd3ba9bf992767106656dec235ce7b81
AayushMandhyani <68859106+AayushMandhyani@users.noreply.github.com>
2020-10-14 19:42:25 +0530

    Add files via upload

---
+++ AngryBirdsStage4-master/Slingshot.js
@@ -0,0 +1,45 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+
+        this.sling1 = loadImage("sprites/sling1.png")
+        this.sling2 = loadImage("sprites/sling2.png")
+        this.sling3 = loadImage("sprites/sling3.png")
+
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+
+    attach(body){
+        this.sling.bodyA = body
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            stroke(42,22,8);
+            line(pointA.x - 15, pointA.y + 10, pointB.x + 20, pointB.y);
+            line(pointA.x - 15, pointA.y + 10, pointB.x - 20, pointB.y);
+
+            image(this.sling3,pointA.x - 25, pointA.y - 5, 15, 30)
+
+        }
+
+        image(this.sling1,170,20)
+        image(this.sling2,140,12)
+
+    }
+    
+}
